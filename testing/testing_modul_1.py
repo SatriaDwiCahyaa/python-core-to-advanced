70500ef66b61aee1745213a069d93f61a5305faa
SatriaDwiCahyaa <zatriagithub@gmail.com>
2026-03-02 14:42:33 +0700

    test: perulangan do-while

--- testing/testing_modul_1.py
+++ testing/testing_modul_1.py
@@ -1,8 +1,16 @@
-for nomor_ke in range(1, 10):
+batas_perulangan = 5
+nomor_ke = 1
+
+while True:
     print(f"Perulangan ke-{nomor_ke}")
+    nomor_ke += 1
+
+    if nomor_ke > batas_perulangan:
+        break
 
 # Output:
 # Perulangan ke-1
 # Perulangan ke-2
-# ...
-# Perulangan ke-9
+# Perulangan ke-3
+# Perulangan ke-4
+# Perulangan ke-5
