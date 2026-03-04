67e06aeb38b041f95503f8a99924799211fabcba
SatriaDwiCahyaa <zatriagithub@gmail.com>
2026-03-04 23:46:36 +0700

    refactor: merapikan teks input dan hapus input penahan console

--- praktikum/algorithms_programming_II/modul_1/praktikum_live_coding.py
+++ praktikum/algorithms_programming_II/modul_1/praktikum_live_coding.py
@@ -4,21 +4,18 @@ os.system('cls')
 
 def main():
 
-    banyak_data = int(input('Masukkan berapa angka yang ingin dimasukkan: '))
+    banyak_data = int(input('Masukkan berapa angka yang ingin dihitung: '))
 
     total = 0
 
     for x in range(1, banyak_data + 1):
-        nilai = float(input(f'Masukkan Angka {x}: '))
+        nilai = float(input(f'Masukkan Angka ke-{x}: '))
         total += nilai
 
     rata_rata = total / banyak_data
 
 
-    print(f'Nilai rata-rata: {rata_rata:.2f}')
-
-    input()
-
+    print(f'\nNilai rata-rata: {rata_rata:.2f}')
 
 if __name__ == '__main__':
     main()
