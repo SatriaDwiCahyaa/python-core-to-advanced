b8af8e839aea2f341e300854e75d4c750f1c595d
SatriaDwiCahyaa <zatriagithub@gmail.com>
2026-03-12 10:28:59 +0700

    refactor: ubah indentasi ke space = 4

--- praktikum/algorithms_programming_II/modul_3/binary_search.py
+++ praktikum/algorithms_programming_II/modul_3/binary_search.py
@@ -3,21 +3,21 @@
 # =================
 
 def binarySearch(sumber_data, target):
-  kiri = 0
-  kanan = len(sumber_data) - 1
+    kiri = 0
+    kanan = len(sumber_data) - 1
 
-  while kiri <= kanan:
-    tengah = (kiri + kanan) // 2
+    while kiri <= kanan:
+        tengah = (kiri + kanan) // 2
 
-    if sumber_data[tengah] == target:
-      return tengah
+        if sumber_data[tengah] == target:
+            return tengah
 
-    if sumber_data[tengah] < target:
-      kiri = tengah + 1
-    else:
-      kanan = tengah - 1
+        if sumber_data[tengah] < target:
+            kiri = tengah + 1
+        else:
+            kanan = tengah - 1
 
-  return -1
+    return -1
 
 def main():
     data = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19] # Data harus urut ascending/descending - binary search
