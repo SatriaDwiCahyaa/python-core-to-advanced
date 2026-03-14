8573c99506f0da43d078444ad01038a0a0e123e9
SatriaDwiCahyaa <zatriagithub@gmail.com>
2026-03-14 11:37:07 +0700

    latihan: R3 tugas modul 3 - Searching Algorithm

--- training_ground/second_area.py
+++ training_ground/second_area.py
@@ -3,65 +3,57 @@ os.system('cls')
 
 
 # --- TUGAS 1 ---
-produk = ['Jaguar', 'Lamborghini', 'Honda', 'Audi', 'Suzuki', 
+PRODUK = ['Jaguar', 'Lamborghini', 'Honda', 'Audi', 'Suzuki', 
           'Mazda', 'Daihatsu', 'Ford', 'Hyundai','Mitsubishi' 
           ]
 
-harga = [ 1_340_000_000, 34_500_000_000, 350_000_000, 2_000_000_000, 
+HARGA = [ 1_340_000_000, 34_500_000_000, 350_000_000, 2_000_000_000, 
          245_000_000, 500_000_000, 169_000_000, 789_000_000, 122_900_000, 278_100_000
          ]     
 
-# --- Tugas 2 ----
-def tampilkan_audi():
+
+def display_audi():
     target = 'Audi'
-    ditemukan = False
-    print('=' * 45)
-    print(f'{'':<15}Produk {target}')
-    print('=' * 45)
+    founded = False
 
-    for indeks, data in enumerate(produk):
-        if data == target:
-            print(f'\n Produk {target} Ditemukan!')
-            print(f' Lokasi di array  : indeks ke-{indeks}')
-            print(f' Harga            : Rp{harga[indeks]:,.0f}')
-            ditemukan = True
-            break
-
-    if not ditemukan:
-        print(f'\nProduk {target} tidak ditemukan')
-
-# --- Tugas 3 ---    
-def sorting_cheap_expensive():
-    print('=' * 45)
-    print(f'{'':<7}Urutan Produk Termurah - Mahal')
-    print('=' * 45)
-
-    # Memasangkan nama produk dengan harga
-    pasangan_data = list(zip(harga, produk))
+    for indeks, product in enumerate(PRODUK):
+        if product == target:
     
-    # Mengurutkan menggunakan bubble sort
-    pasangan_data.sort(key=lambda x: x[0])
+            print(f'Produk {target} ditemukan!')
+            print(f'\nProduk {target} pada indeks ke-{indeks} atau urutan ke-{indeks + 1}')
+            print(f'\nHarga produk Rp{HARGA[indeks]:,.0f}')
+            founded = True
 
-    for rank, (h, p) in enumerate(pasangan_data, start=1):
-        print(f'{rank:<5} {p:<15} Rp {h:,.0f} ')
+    if not founded:
+        print(f'Produk {target} tidak ditemukan pada data.')
 
-    return pasangan_data
 
-# --- Tugas 4 ---
-def second_cheap_produk(pasangan_data): 
-    print('=' * 45)
-    print(f'{'':<7}Produk Termurah Nomor 2')
-    print('=' * 45)
+def pengurutan_ascending():
+    pasangan = list(zip(HARGA, PRODUK))
+    pasangan.sort(key=lambda x: x[0])
 
-    h_termurah_2 = pasangan_data[1]
+    print(f'Rank{'':<2} Produk {'':<10} Harga')
+    print(f'{'-'*4} {'-'*14} {'-'*20}')
+    
+    for indeks, (h, p) in enumerate(pasangan, start=1):
+        print(f'{indeks:<6} {p:<13} Rp {h:,.0f}')
 
-    print(f'Produk Termurah ke-2 : {h_termurah_2[1]:<10} Rp {h_termurah_2[0]:,.0f}')
+    return pasangan
+
+def request_display(pasangan_data):
+    target = 2
+    
+    for indeks, (h, p) in enumerate(pasangan_data, start=1):
+        if indeks == target:
+            print('=' * 45)
+            print(f'{'':<10} Harga Produk Termurah')
+            print('=' * 45)
+            print(f'{indeks:<3} {p:<13} Rp {h:,.0f}')
 
 def main():
-    tampilkan_audi()
+    hasil = pengurutan_ascending()
+    request_display(hasil)
+    input()
 
-    hasil = sorting_cheap_expensive()
-
-    second_cheap_produk(hasil)
-
-main()
+if __name__ == "__main__":
+    main()
